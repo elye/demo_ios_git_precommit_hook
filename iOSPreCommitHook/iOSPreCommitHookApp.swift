914aeb5812efb085131abab25f57b0bf83a98672
elye <elye.project@gmail.com>
2025-09-10 23:23:50 +1000

    Initial commit

---
+++ iOSPreCommitHook/iOSPreCommitHookApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSPreCommitHookApp.swift
+//  iOSPreCommitHook
+//
+//  Created by Elisha Lye on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSPreCommitHookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
